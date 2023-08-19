51b5b4a364c0f4bbd312d6b9c43e1d717fdd6cb1
Ellie Korth <87391629+ellsk21@users.noreply.github.com>
2023-08-19 17:30:33 +1200

    updated weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,48 @@
 import React from "react";
 import axios from 'axios';
+import "./Weather.css"
 
+        export default function Weather () {
+            return (
+         <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                       <input 
+                       type="search"
+                       placeholder="Type a city..."
+                       id="city-input"
+                       autocomplete="off" />
 
-export default function Weather()
-{
-    return (
-        <h2>hello from weather</h2>
-    )
-}
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" class="btn btn-primary" />
+                    </div> 
+                </div>
+            </form>
+            <div class="overview">
+                <h1 id="city">Mangawhai</h1>
+                <ul>
+                    <li><span id="date">Aug 19 2023</span></li>
+                    <li id="description">Partly Cloudy</li>
+                </ul>
+            </div>
+            <div className="row">
+            <div className="col-6">
+                <div className="d-flex weather-temperature">
+                    <img 
+                    src="https://s.yimg.com/os/weather/1.0.1/shadow_icon/60x60/fair_day@2x.png"
+                    alt="partly cloudy"
+                    id="icon" />
+                    <div>
+                        <strong id="temperature">
+                            <span className="units"> 11°C</span>
+                        </strong>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            </div>
+            )
+        }
